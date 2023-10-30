f80a58bd0431a5670737371c81ebbb81b9b7e710
yanshouwang <yanshouwang@outlook.com>
2023-10-30 11:27:41 +0800

    fix: 优化代码

--- lib/log_service.dart
+++ lib/log_service.dart
@@ -1,9 +1,7 @@
 /// A Dart package for simplify the usage of the official `logging` package.
 library;
 
-export 'package:ansicolor/ansicolor.dart';
 export 'package:logging/logging.dart';
 
-export 'src/terminal.dart';
 export 'src/log_controller.dart';
 export 'src/log_service.dart';
